69f159c2ae2d46b42a2e16b7dddd675fdf558f48
gh.hassanzadeh <gholamreza15402@gmail.com>
2024-05-27 10:19:24 +0330

    create mui configs adn rtl

--- configs/app-configurations/emotion/emotion.config.tsx
+++ configs/app-configurations/emotion/emotion.config.tsx
@@ -1,8 +1,19 @@
 // * Import Casual modules
-import { FC } from 'react';
+import { FC } from "react";
 
 // * Import Tools
-import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
+import { ThemeProvider as EmotionThemeProvider } from "@emotion/react";
 
 // * import interfaces
-import { IEmotionConfig } from '@/configs/app-configurations/emotion/emotion.config.interface';
+import { IEmotionConfig } from "@/configs/app-configurations/emotion/emotion.config.interface";
+
+// * import constants
+import { THEME_LIGHT } from "@/constants/themes";
+
+export const EmotionConfig: FC<IEmotionConfig> = ({ children }) => {
+    return (
+        <EmotionThemeProvider theme={THEME_LIGHT}>
+            {children}
+        </EmotionThemeProvider>
+    );
+};
---
+++ configs/app-configurations/mui/material-ui.config.theme.ts
@@ -0,0 +1,43 @@
+// * Import Tools
+import { createTheme, Theme } from '@mui/material/styles';
+
+
+// * Import Constants
+import { THEME_LIGHT } from '@/constants/themes';
+
+
+const { palette: { augmentColor } } = createTheme();
+
+const createColor = (mainColor: string) => augmentColor({ color: { main: mainColor } })
+
+
+export const MATERIAL_UI_THEME_LIGHT_CONFIG: Theme = createTheme({
+    direction: 'ltr',
+    typography: {
+        fontFamily: 'Roboto',
+        button: {
+            fontSize: '1rem',
+        }
+
+    },
+    components: {
+        // Name of the component
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    fontSize: '1rem',
+                },
+            },
+        },
+        MuiTextField: {
+            styleOverrides: {
+
+                root: {
+
+                    fontFamily: "Roboto",
+                    width: "100%",
+                }
+            }
+        },
+    }
+})
