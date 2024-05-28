b1cf0e3713813b04703762166b4f0b276e1d8cb0
gh.hassanzadeh <gholamreza15402@gmail.com>
2024-05-28 11:20:31 +0430

    create flexbox componet

---
+++ components/atoms/flex/flex.atom.tsx
@@ -0,0 +1,18 @@
+// * import tools
+import React, { FC } from "react";
+import styled from "@emotion/styled";
+
+// *
+import { IFlex } from "@/components/atoms/flex/Flex.atom.interface";
+
+const Flex = styled.div<IFlex>`
+    display: flex;
+    flex-direction: ${({ direction }) => direction || "row"};
+    justify-content: ${({ justify }) => justify || "flex-start"};
+    align-items: ${({ align }) => align || "stretch"};
+    flex-wrap: ${({ wrap }) => wrap || "nowrap"};
+    gap: ${({ gap }) => gap || "0"};
+`;
+export const FlexAtom: FC<IFlex> = ({ children, ...rest }) => {
+    return <Flex {...rest}>{children}</Flex>;
+};
---
+++ components/atoms/index.tsx
@@ -0,0 +1,1 @@
+export { FlexAtom as Flex } from "@/components/atoms/flex/flex.atom";
