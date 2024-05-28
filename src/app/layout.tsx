12a526f7d45e125689746b81c681b96402786364
gh.hassanzadeh <gholamreza15402@gmail.com>
2024-05-28 11:15:57 +0430

    made in lgon page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { AppConfigurations } from "@/configs/app-configurations/app.configurations";
 
-import "../globals.css"; // اضافه کردن فایل CSS عمومی
 
 export default function RootLayout({
     children,
